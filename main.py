9767757935fc4861fc8092148ba6f79fdc8f39fb
Zach Brown <zachbrown@Zachs-MBP.attlocal.net>
2020-05-27 10:41:27 -0700

    Fixed shell of sign in page

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template #, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
 
-# @app.route("/signin", methods = ["GET", "POST"])
-# def signIn():
-#     name = request.form["name"]
-#     member_id = request.form["member_id"]
-#     # print(f"{name} {member_id}")
-#     return render_template("home.html")
+@app.route("/") #, methods = ["GET", "POST"])
+def signIn():
+    # name = request.form["name"]
+    # member_id = request.form["member_id"]
+    # print(f"{name} {member_id}")
+    return render_template("signin.html")
 
 @app.route("/about")
 def about():
@@ -19,7 +19,7 @@ def about():
 
 @app.route("/items")
 def items():
-    return {"Diamond": 1000, "Sphinx": 400}
+    return render_template("items.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
