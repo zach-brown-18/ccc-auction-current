30c1984bca67413f60b48ebc73e84ee2abc3f719
Zach Brown <zachbrown@Zachs-MBP.attlocal.net>
2020-05-26 17:06:58 -0700

    Fixing sign in page

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["POST"])
 def home():
     return render_template("home.html")
 
