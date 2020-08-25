834a16986656da7b4e4517ff9007eca8712222bf
wangye <coconut@wangyedeAir.attlocal.net>
2020-08-25 15:03:07 -0700

    Changed static path back to static/

--- ccc_auction/__init__.py
+++ ccc_auction/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bafcdaac27ca2fbcc30e6e3543810b11'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auction.db'
 app.static_folder = 'static'
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='https://ccc-acution.herokuapp.com/static/')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
