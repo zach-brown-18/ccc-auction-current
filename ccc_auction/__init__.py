a542b22dfdaaaa771799ce7ce643c8fd03ee8069
wangye <coconut@wangyedeAir.attlocal.net>
2020-08-25 15:46:38 -0700

    fixing item image files

--- ccc_auction/__init__.py
+++ ccc_auction/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from whitenoise import WhiteNoise
-import os
+# from whitenoise import WhiteNoise
+# import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bafcdaac27ca2fbcc30e6e3543810b11'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auction.db'
 app.static_folder = 'static'
-app.wsgi_app = WhiteNoise(app.wsgi_app, root=os.path.join(os.path.dirname(__file__), 'static'), prefix='static/')
+# app.wsgi_app = WhiteNoise(app.wsgi_app, root=os.path.join(os.path.dirname(__file__), 'static'), prefix='static/')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
